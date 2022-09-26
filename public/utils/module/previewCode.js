f670ca13a1c2d54a234f905ad30fd21120045240
lijinbo-LX <lijinbode@foxmail.com>
2022-09-26 10:04:15 +0800

    previewCode样式调整

--- public/utils/module/previewCode.js
+++ public/utils/module/previewCode.js
@@ -3,23 +3,36 @@
  * @param {string} fileContent
  */
 export async function previewCode(fileContent) {
-  const mdFile = new File([fileContent], 'test.md', { type: 'text/markdown;charset=utf-8' })
+  const mdFile = new File([fileContent], 'code.md', { type: 'text/markdown;charset=utf-8' })
   const filePath = URL.createObjectURL(mdFile)
 
   const codeContainer = document.createElement('div')
+  const shadow = codeContainer.attachShadow({ mode: 'open' })
+
+  const style = document.createElement('style')
+  style.innerHTML = `
+  .btn {
+    color: #fff;
+    cursor: pointer;
+    background-color: #409eff;
+    border: 1px solid #dcdfe6;
+    box-sizing: border-box;
+    padding: 8px 12px;
+    margin: 12px;
+    font-size: 14px;
+    border-radius: 4px;
+    transition: 0.3s;
+  }
+  .btn:hover {
+    background: #66b1ff;
+    border-color: #66b1ff;
+    transition: 0.3s;
+  }
+  `
+  shadow.append(style)
   const iframe = document.createElement('iframe')
   const button = document.createElement('button')
-  button.style = `
-    color:#fff;
-    cursor:pointer;
-    background-color:#409eff;
-    border:1px solid #dcdfe6;
-    box-sizing:border-box;
-    padding:8px 12px;
-    margin:12px;
-    font-size:14px;
-    border-radius:4px;
-  `
+  button.className = 'btn'
   button.innerText = '查看代码'
   let isOpen = false
   let isInit = true
@@ -33,14 +46,30 @@ export async function previewCode(fileContent) {
     if (isInit) {
       iframe.style = 'width:90vw;height:90vh;border:none;'
       iframe.src = `/common/parseMarked/parseMarked.html?filePath=${filePath}`
-      codeContainer.append(iframe)
+      shadow.append(iframe)
       isInit = false
     }
     isOpen = true
     button.innerText = '收起代码'
     iframe.style.display = 'block'
   }
-  codeContainer.append(button)
+  shadow.append(button)
+
   const stx = document.getElementById('stx') || document.body
   stx.append(codeContainer)
+}
+/**
+ * 预览文件列表
+ * @param {string[]} pathList
+ */
+export async function previewCodeFileList(pathList) {
+  const mdStrList = []
+  let index = 1
+  for (let item of pathList) {
+    const response = await fetch(item)
+    const code = await response.text()
+    const fileType = item.split('.').slice(-1)[0]
+    mdStrList.push(`# ${index++} ${decodeURIComponent(item)}\n` + '```' + `${fileType}\n${code}\n` + '```')
+  }
+  previewCode(mdStrList.join('\n\n'))
 }
