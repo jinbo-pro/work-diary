234fb14e6f3c0cb1dcb932c76081b1628ce9cc00
lijinbo <lijnbode@foxmail.com>
2022-10-26 22:59:20 +0800

    base64文件下载

--- public/collection/001-100/045-文件转base64/045-main.js
+++ public/collection/001-100/045-文件转base64/045-main.js
@@ -1,4 +1,6 @@
-// base64/buffer 解析
+import { copyText } from '/utils/page.js'
+
+/**base64/buffer 解析 */
 function fileParse(file, type = 'base64') {
   return new Promise((resolve) => {
     let fileRead = new FileReader()
@@ -13,34 +15,54 @@ function fileParse(file, type = 'base64') {
   })
 }
 
-// 复制文字到剪切板
-function copyText(text) {
-  if (!text) return
-  // 复制借助input实现
-  document.execCommand('Copy')
-  var inputZ = document.createElement('input')
-  inputZ.setAttribute('id', 'inputCopy')
-  inputZ.value = text
-  document.body.appendChild(inputZ)
-  document.getElementById('inputCopy').select()
-  document.execCommand('Copy')
-  document.body.removeChild(inputZ)
+function downFileByUrl(imgUrl, fileName) {
+  const a = document.createElement('a')
+  a.href = imgUrl
+  // 默认截取 url 末尾为文件名
+  a.download = fileName || imgUrl.split('/').slice(-1)[0]
+  a.click()
 }
 
-var copy = document.getElementById('copy')
-var result = document.getElementById('result')
-var selectFile = document.getElementById('selectFile')
-selectFile.onchange = function (e) {
-  var files = e.target.files
-  var file = files[0]
-  fileParse(file).then((res) => {
-    console.log(res, '-->>> res')
-    result.innerHTML = res
-  })
+function getBase64Name(base64) {
+  const [n] = base64.split(';')
+  return n.replace(/:/, '_').replace(/\//, '.')
 }
 
-copy.onclick = function () {
-  var text = result.innerHTML
-  copyText(text)
-  alert('复制成功')
-}
+new Vue({
+  el: '#app',
+  data() {
+    return {
+      base64: ''
+    }
+  },
+  computed: {
+    isImage() {
+      return /^data:image/.test(this.base64)
+    }
+  },
+  methods: {
+    selectFile(e) {
+      const files = e.target.files
+      fileParse(files[0]).then((res) => {
+        this.base64 = res
+      })
+    },
+    copy() {
+      copyText(this.base64)
+      this.$message.success('复制成功')
+    },
+    base64FileDown() {
+      let arr = this.base64.split(','),
+        mime = arr[0].match(/:(.*?);/)[1],
+        bstr = atob(arr[1]),
+        n = bstr.length,
+        u8arr = new Uint8Array(n)
+      while (n--) {
+        u8arr[n] = bstr.charCodeAt(n)
+      }
+      const blob = new Blob([u8arr], { type: mime })
+      const fileUrl = URL.createObjectURL(blob)
+      downFileByUrl(fileUrl, getBase64Name(this.base64))
+    }
+  }
+})
