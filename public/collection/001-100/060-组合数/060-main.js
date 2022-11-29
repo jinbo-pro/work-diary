4a7c88856a393ccc9b7825caa413135e2a16f685
lijinbo-LX <lijinbode@foxmail.com>
2022-11-29 15:34:15 +0800

    数理化公式编辑器

--- public/collection/001-100/060-组合数/060-main.js
+++ public/collection/001-100/060-组合数/060-main.js
@@ -1,20 +1,29 @@
 console.log('060-组合数')
 
 function computed(n, m) {
-  var a = 1
-  for (var i = n; i > n - m; i--) {
+  if (m == 0) return 1
+  let a = 1
+  for (let i = n; i > n - m; i--) {
     a *= i
   }
-  var b = 1
-  for (var i = 1; i <= m; i++) {
+  let b = 1
+  for (let i = 1; i <= m; i++) {
     b *= i
   }
   return a / b
 }
 
-$('#handel').on('click', function () {
-  var n = parseInt($('#n').val())
-  var m = parseInt($('#m').val())
-  var c = computed(n, m)
-  $('#result').text(c)
+new Vue({
+  el: '#app',
+  data() {
+    return {
+      m: 0,
+      n: 0
+    }
+  },
+  computed: {
+    result() {
+      return this.m && this.n ? computed(this.n, this.m) : ''
+    }
+  }
 })
