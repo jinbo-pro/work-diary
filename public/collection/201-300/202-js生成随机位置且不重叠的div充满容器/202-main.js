c9707dfb3375b5685554f9dc16c3c6f180cdecf7
lijinbo <lijnbode@foxmail.com>
2022-12-10 23:17:50 +0800

    消息提示

--- public/collection/201-300/202-js生成随机位置且不重叠的div充满容器/202-main.js
+++ public/collection/201-300/202-js生成随机位置且不重叠的div充满容器/202-main.js
@@ -13,10 +13,10 @@ function createPoint(w, h, count, domW) {
   // 已存在的点
   const pointList = []
   // 最大填充数量
-  const maxCount = (w / domW) * (h / domW) * 0.6
+  const maxCount = ~~((w / domW) * (h / domW) * 0.6)
   if (count > maxCount) {
     count = maxCount
-    console.log(`建议最大数量：${maxCount}`)
+    console.log(`为防止堆栈溢出建议最大数量：${maxCount}`)
   }
   // 检测点是否与其他点发生重叠
   const checkPointOverlap = (x, y) => {
@@ -30,9 +30,9 @@ function createPoint(w, h, count, domW) {
   while (pointList.length < count) {
     const x = ra(w)
     const y = ra(h)
-    if (checkPointOverlap(x, y)) continue
     // 检测点是否超出边界
     if (x + domW > w || y + domW > h) continue
+    if (checkPointOverlap(x, y)) continue
     pointList.push({ x, y })
   }
   return pointList
@@ -56,13 +56,17 @@ new Vue({
       const dom = document.getElementById('container')
       const w = dom.clientWidth
       const h = dom.clientHeight
+      console.time('createTime：')
       const pointList = createPoint(w, h, this.count, this.domSize)
+      console.timeEnd('createTime：')
       this.list = pointList.map((e, i) => {
         return {
           name: i + 1,
-          position: {
+          style: {
             top: e.y + 'px',
-            left: e.x + 'px'
+            left: e.x + 'px',
+            width: this.domSize + 'px',
+            height: this.domSize + 'px'
           }
         }
       })
