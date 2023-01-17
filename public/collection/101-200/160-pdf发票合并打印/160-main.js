c2d143b43b6fa85e756b900f457c9aa35b7119a1
lijinbo-LX <lijinbode@foxmail.com>
2023-01-17 16:48:54 +0800

    pdf发票合并打印样式调整

--- public/collection/101-200/160-pdf发票合并打印/160-main.js
+++ public/collection/101-200/160-pdf发票合并打印/160-main.js
@@ -1,43 +1,90 @@
-console.log('160-pdf发票合并打印')
+import { arrayChunk } from '/utils/collect.js'
 
-const container = document.getElementById('container')
-async function main(files) {
-  for (let file of files) {
-    const url = URL.createObjectURL(file)
-    /**字体解析文件地址 */
-    const cMapUrl = 'https://cdn.jsdelivr.net/npm/pdfjs-dist@2.16.105/cmaps/'
-    const pdfFile = await pdfjsLib.getDocument({ url, cMapUrl }).promise
-    // 发票只解析第一页即可
-    const page = await pdfFile.getPage(1)
-    // 解析PDF
-    const viewport = page.getViewport({ scale: 3 }) // 页面缩放比例
-    const canvas = document.createElement('canvas')
-    canvas.className = 'canvas_box'
+new Vue({
+  el: '#app',
+  data() {
+    return {
+      isPrint: false,
+      direction: 1,
+      viewCount: 2,
+      w: 770,
+      h: 1030
+    }
+  },
+  watch: {
+    direction(v) {
+      const printStyle = document.getElementById('printStyle')
+      if (v == 1) {
+        this.w = 700
+        this.h = 1030
+        printStyle.innerHTML = '@page {margin: 0;size: portrait;}'
+      } else {
+        this.w = 1200
+        this.h = 800
+        printStyle.innerHTML = '@page {margin: 0;size: landscape;}'
+      }
+    }
+  },
+  methods: {
+    async selectFile(e) {
+      const container = document.getElementById('container')
+      const viewList = arrayChunk(Array.from(e.target.files), this.viewCount)
+      for (let view of viewList) {
+        const div = document.createElement('div')
+        div.className = 'view'
+        div.style.width = this.w + 'px'
+        div.style.height = this.h + 'px'
+        for (let file of view) {
+          const url = URL.createObjectURL(file)
+          /**字体解析文件地址 */
+          const cMapUrl = 'https://cdn.jsdelivr.net/npm/pdfjs-dist@2.16.105/cmaps/'
+          const pdfFile = await pdfjsLib.getDocument({ url, cMapUrl }).promise
+          // 发票只解析第一页即可
+          const page = await pdfFile.getPage(1)
+          // 解析PDF
+          const viewport = page.getViewport({ scale: 3 }) // 页面缩放比例
+          const canvas = document.createElement('canvas')
 
-    // 设置canvas真实宽高
-    canvas.width = viewport.width
-    canvas.height = viewport.height
+          // 设置canvas真实宽高
+          canvas.width = viewport.width
+          canvas.height = viewport.height
 
-    const context = canvas.getContext('2d')
+          const p = viewport.width / viewport.height
+          let cw = viewport.width
+          let ch = viewport.height
 
-    // 渲染生成
-    page.render({
-      canvasContext: context,
-      viewport: viewport
-    })
-    container.append(canvas)
+          if (this.viewCount == 1) {
+            cw = this.w
+            ch = this.w / p
+          } else if (this.viewCount == 2) {
+            const h = this.h / 2 - 20
+            cw = h * p
+            ch = h
+          } else {
+            const w = this.w / 2 - 20
+            cw = w
+            ch = w / p
+          }
+          canvas.style.width = cw + 'px'
+          canvas.style.height = ch + 'px'
+          const context = canvas.getContext('2d')
+
+          // 渲染生成
+          page.render({
+            canvasContext: context,
+            viewport: viewport
+          })
+          div.append(canvas)
+        }
+        container.append(div)
+      }
+    },
+    async printHandle() {
+      document.title = new Date().toLocaleDateString().replace(/\//g, '-') + '发票打印'
+      this.isPrint = true
+      await this.$nextTick()
+      print()
+      this.isPrint = false
+    }
   }
-}
-document.getElementById('pdfFile').onchange = function (e) {
-  const files = e.target.files
-  main(files)
-}
-document.getElementById('print').onclick = function () {
-  document.title = new Date().toLocaleDateString().replace(/\//g, '-') + '发票打印'
-  printJS({
-    printable: 'container',
-    type: 'html',
-    targetStyles: ['*'],
-    style: '@page{size:auto;margin: 0;}' //去除页眉页脚
-  })
-}
+})
