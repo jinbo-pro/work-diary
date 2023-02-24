aeb7f650326d3242cd2cd99689b8141917502c00
lijinbo-LX <lijinbode@foxmail.com>
2023-02-24 10:16:49 +0800

    uni-app搭建html5容器应用

---
+++ public/collection/201-300/223-uni-app搭建html5容器应用/view/web-view.vue
@@ -0,0 +1,20 @@
+<template>
+  <view>
+    <web-view src="http://localhost:7586/h5-page.html" @message="handleMessage"></web-view>
+  </view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+
+  methods: {
+    // webview向外部发送消息
+    handleMessage(e) {
+      console.log('接收到消息：' + JSON.stringify(e.detail.data))
+    }
+  }
+}
+</script>
