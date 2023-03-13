44cb3fb3903b455112c9ff409719914417f6cd7c
lijinbo-LX <lijinbode@foxmail.com>
2023-03-13 09:11:24 +0800

    归档整理socket

--- build/index.js
+++ build/index.js
@@ -10,7 +10,7 @@ function resolve(dir) {
 async function main() {
   // 删除上传的文件
   clearDir(resolve('public/upload'))
-  // diary
+  // server
   await esbuild.build({
     entryPoints: [resolve('app/index.js')],
     platform: 'node',
@@ -19,16 +19,8 @@ async function main() {
     bundle: true,
     minify: true
   })
+  // web
   await copyDir(resolve('public'), resolve('dist/public'))
-  // websocket
-  await esbuild.build({
-    entryPoints: [resolve('app/websocket/socket.js')],
-    platform: 'node',
-    target: ['node16'],
-    outfile: resolve('dist/app/websocket.js'),
-    bundle: true,
-    minify: true
-  })
   // 添加打包时间
   const home = fs.readFileSync(resolve('public/index.html')).toString()
   const code = `${home}\n<script>console.log('${new Date().toLocaleString()}');</script>`
