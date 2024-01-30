560da4941d65653eac55f4ec4f21d7a38975d64a
lijinbo <lijinbode@foxmailcom>
2024-01-30 09:38:20 +0800

    依赖更新

--- build/build.js
+++ build/build.js
@@ -6,17 +6,19 @@ const { resolve } = require('./utils')
 const createConfig = require('./createConfig')
 
 ;(async () => {
-  // 打包页面
-  await build(createConfig('production'))
-  // 打包服务器
-  await esbuild.build({
-    entryPoints: [resolve('app/index.js')],
-    platform: 'node',
-    target: ['node16'],
-    outfile: resolve('dist/app/index.js'),
-    bundle: true,
-    minify: true
-  })
+  await Promise.all([
+    // 打包页面
+    build(createConfig('production')),
+    // 打包服务器
+    esbuild.build({
+      entryPoints: [resolve('app/index.js')],
+      platform: 'node',
+      target: ['node16'],
+      outfile: resolve('dist/app/index.js'),
+      bundle: true,
+      minify: true
+    })
+  ])
   // 添加打包信息
   const indexPath = resolve('dist/public/index.html')
   const home = fs.readFileSync(indexPath).toString()
