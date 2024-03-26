f5565ce1030ac8a19a59af4d0b77c695cadccaa7
lijinbo <lijinbode@foxmailcom>
2024-03-26 15:04:15 +0800

    concurrencyTask重构

--- app/utils/concurrencyTask.js
+++ app/utils/concurrencyTask.js
@@ -1,75 +1,43 @@
 /**
- * 并发任务管理器
+ * 并发任务组
  * @param {Array<() => Promise<any>>} tasks 任务数组
- * @param {number} limit 并发数
- * @returns
+ * @param {number} limit 最大并发数
  */
-async function concurrencyTask(tasks, limit = 1) {
-  const runTask = async (fnList) => {
-    if (!fnList || !fnList.length) {
-      return null
+function concurrencyTask(tasks, limit = 2) {
+  const len = tasks.length
+  if (len === 0) return Promise.resolve([])
+  return new Promise((resolve) => {
+    let index = 0
+    let count = 0
+    const result = []
+    function run() {
+      const i = index
+      const task = tasks[index]
+      index++
+      task()
+        .then((res) => {
+          result[i] = res
+        })
+        .catch((e) => {
+          result[i] = e
+        })
+        .finally(() => {
+          count++
+          if (count === len) {
+            resolve(result)
+          }
+          if (index < len) {
+            run()
+          }
+        })
     }
-    try {
-      const pms = fnList.map((fn) => fn())
-      return await Promise.all(pms)
-    } catch (error) {
-      console.log(error, `执行出错 x-x`)
-      return null
+    for (let i = 0; i < Math.min(limit, len); i++) {
+      run()
     }
-  }
-  const result = []
-  for (let i = 0, len = tasks.length; i < len; i += limit) {
-    const value = await runTask(tasks.slice(i, i + limit))
-    result.push(value)
-  }
-  return result
+  })
 }
 
-/**
- * 并发下载任务
- * @param {Array<() => Promise<any>>} tasks 任务数组
- * @param {number} limit 并发数
- */
-async function queueDowload(list, limit = 3) {
-  let concurrentDownloads = 0 // 当前并发下载数
-  let taskList = new Set()
-  for (let fn of list) {
-    // 如果当前并发下载数已达到最大限制，等待一个下载任务完成后再继续
-    while (concurrentDownloads >= limit) {
-      await new Promise((a) => setTimeout(a, 300))
-    }
-    concurrentDownloads++
-    const pms = fn()
-    taskList.add(pms)
-    pms.finally(() => {
-      taskList.delete(pms)
-      concurrentDownloads--
-    })
-  }
-  if (taskList.size) {
-    return Promise.all([...taskList])
-  }
-}
-
-/**
- * 1. 更加完善的队列管理器可以使用 async 库里面的 queue
- *
- * async/queue
- * https://unpkg.com/async@3.2.4/queue.js
- * other documents
- * https://my.oschina.net/huangsz/blog/176203
- * 
- * 2. 轻便好用的队列控制器可以使用 p-limit 库
- * p-limit
- * https://www.npmjs.com/package/p-limit
- * other documents
- * https://zhuanlan.zhihu.com/p/549829103
- */
-
-module.exports = {
-  concurrencyTask,
-  queueDowload
-}
+module.exports = concurrencyTask
 
 /**
  * 使用示例
@@ -78,27 +46,11 @@ module.exports = {
  for (let i = 0; i < 10; i++) {
    const fn = () => fetch('xxx').then((res) => {
      console.log(i, '-->>> i')
+     return res
    })
    list.push(fn)
  }
  concurrencyTask(list, 3).then((res) => {
    console.log(res, '-->>> 执行完毕')
  })
- // 或者
- queueDowload(list, 3).then((res) => {
-   console.log(res, '-->>> 执行完毕')
- })
- */
-
-/**
- * 特点：可以每次只会同时下载最多3个文件，
- * 当有一个下载任务完成后，会开始下一个等待中的下载任务。
- * 通过控制并发下载数量，可以避免同时下载过多文件导致内存溢出的问题。
- *
- * 两个方法异同
- * 如果异步任务的时间短-比如小文件下载建议使用 concurrencyTask
- * 如果异步任务的时间长-比如大文件下载建议使用 queueDowload
- *
- * 长时间异步任务时 concurrencyTask 的问题是一个任务小组里面如果存在一个任务的下载时间很长那么整个小组都会等待他，而 queueDowload 则会灵活的加入下一个任务
- * 短时间异步任务时 queueDowload 的任务等待会有 300 毫秒的延迟，导致下载偏慢
  */
